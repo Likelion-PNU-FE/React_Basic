aa358b901084cc8c02d00fc894e789526d668872
ChoiJuyoung <cjy020718@gmail.com>
2024-07-04 12:16:35 +0900

    feat: create react app, 초기 설정, components 추가

---
+++ my-app/project02/src/App.js
@@ -0,0 +1,15 @@
+import Title from "./components/Title";
+import InputBox from "./components/InputBox";
+import TodoLists from "./components/TodoLists";
+
+function App() {
+  return (
+    <div className="App">
+      <Title />
+      <InputBox />
+      <TodoLists />
+    </div>
+  );
+}
+
+export default App;
