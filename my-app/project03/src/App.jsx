8988d21567644d32bf183a619798c819767eea43
ChoiJuyoung <cjy020718@gmail.com>
2024-07-11 16:08:10 +0900

    feat: 화면 구성

---
+++ my-app/project03/src/App.jsx
@@ -0,0 +1,14 @@
+import Search from "./components/Search";
+import MovieList from "./components/MovieList";
+
+function App() {
+  return (
+    <div className="App">
+      {/* <Search /> */}
+      <MovieList />
+      <div>페이지네이션</div>
+    </div>
+  );
+}
+
+export default App;
